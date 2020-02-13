47d1fbd086fd56002c2c78626c1507c702debfca
Carolyn <carolyngychan@gmail.com>
2020-02-12 18:37:29 -0800

    Combination sum

--- combination-sum.py
+++ combination-sum.py
@@ -1,19 +1,24 @@
 # Combination Sum
+# Recursions and such
+# Time: O(n^k) | Space: O(n*k) (???)
 
- def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
-    pass
+from typing import List
 
-def recurse(candidate: List[int], target: int, solutions):
-    if sum(candidates) > target:
-        return
-    if sum(candidates) == target:
-        solutions.append(candidates)
-        return
-    # if sum(candidates) < target
-    
+def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
+    return recurse(sorted(candidates), [], target, [])
+
+def recurse(candidates: List[int], curr_list: int, target: int, solutions: List[int]) -> List[List[int]]:
+    for i, c in enumerate(candidates):
+        if sum(curr_list) + c == target:
+            solutions.append(curr_list + [c])
+        elif sum(curr_list) + c < target:
+            recurse(candidates[i:], curr_list + [c], target, solutions)
+        # if sum(curr_list) + c > target, ignore it
+    return solutions
 
 test_cases = [  ([2, 3, 6, 7], 7),
-                ([2, 3, 5], 8)
+                ([2, 3, 5], 8),
+                ([], 5)
             ]
 for case in test_cases:
     candidates, target = case
