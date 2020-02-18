79aa0cb626a6acd9c186c4c753776fe58e5935ed
Carolyn <carolyngychan@gmail.com>
2020-02-17 18:43:46 -0800

    Maximum product subarray

--- maximum-product-subarray.py
+++ maximum-product-subarray.py
@@ -1,20 +1,28 @@
 # Maximum Product Subarray
+# Time: O(n) | Space: O(1)
 
 from math import inf
 from typing import List
 
 def maxProduct(nums: List[int]) -> int:
-    sol = -inf
-    curr_product = 1
-    neg_indices = []
+    if not(nums):
+        return 0
+    max_prod = -inf
+    # First pass to get "left" negative product
+    curr_prod = 1
     for n in nums:
-            if n == 0:
-                curr_product = 1
-            else:
-                curr_product = curr_product * n
-            sol = max(curr_product, sol)
-            
-    return sol
+        curr_prod = curr_prod * n
+        max_prod = max(max_prod, curr_prod)
+        # If n is 0, reset product
+        curr_prod = 1 if n == 0 else curr_prod
+    # Second pass to get "right" negative product
+    curr_prod = 1
+    for n in reversed(nums):
+        curr_prod = curr_prod * n
+        max_prod = max(max_prod, curr_prod)
+        # If n is 0, reset product
+        curr_prod = 1 if n == 0 else curr_prod
+    return max_prod
         
 cases = [   [2, 3, -2, 4],
             [-2, 0, -1],
