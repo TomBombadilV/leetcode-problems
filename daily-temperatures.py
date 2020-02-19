cb1243a6a74ba88be8f5f5a803e400dc6f25ed95
Carolyn <carolyngychan@gmail.com>
2020-02-18 18:54:51 -0800

    Daily Temperatures

--- daily-temperatures.py
+++ daily-temperatures.py
@@ -1,22 +1,19 @@
 # Daily Temperatures
 # idk I think I'm stupid or mentally impaired
+# Time: O(n) | Space: O(n)
 
 from typing import List
 
 def dailyTemperatures(T: List[int]) -> List[int]:
-    l = []
-    for i in range(len(T)-1):
-        j = 1
-        next_temp = T[i+j]
-        while T[i] >= next_temp and i+j < len(T)-1:
-            j+=1
-            next_temp = T[i+j]
-        if T[i] < next_temp:
-            l.append(j)
-        else:
-            l.append(0)
-    l.append(0)
-    return l
+    stack = []
+    res = [0] * len(T)
+    for i in range(len(T)):
+        if stack and T[i] > stack[-1][0]:
+            while stack and T[i] > stack[-1][0]:
+                _, j = stack.pop()
+                res[j] = i - j
+        stack.append((T[i], i))
+    return(res)
 
 test_cases = [  [73, 74, 75, 71, 69, 72, 76, 73],
                 [0, 0, 0],
