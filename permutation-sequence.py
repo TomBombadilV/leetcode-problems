c4a23ffa4a31df85a7057a81e5a6fb9e5bcba09b
Carolyn <carolyngychan@gmail.com>
2020-02-28 09:49:55 -0800

    Permutation sequence

--- permutation-sequence.py
+++ permutation-sequence.py
@@ -1,20 +1,30 @@
 # Permutation Sequence
+# Do maffs based on pattern of permutation (each number is repeated (n-1)! 
+# times at each level)
 # Time: O(n) | Space: O(n)
 
 from math import factorial
 from typing import List
 
 def getPermutation(n: int, k: int) -> str:
+    # Array of possible nums
     nums = [i + 1 for i in range(n)]
     res = ''
+    # Go to 0-based indexing
     k = k - 1
     fac = factorial(n)
     while nums:
+        # (n - 1)!
         fac = fac // n
+        # Get correct index in possible nums
         i = k // fac
+        # Add to result
         res = res + str(nums[i])
+        # Remove used num from nums
         nums = nums[:i] + nums[i + 1:]
+        # Narrow scope of k
         k = k % fac
+        # Length decreases
         n = n - 1
     return res
 
