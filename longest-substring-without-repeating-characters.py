b0a058f65c55725ce2d00b3ac920aa7e6426bd34
Carolyn <carolyngychan@gmail.com>
2020-03-07 13:30:27 -0800

    Longest Substring Without Repeating Character

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -4,12 +4,11 @@ def lengthOfLongestSubstring(s: str) -> int:
     char_dic = {}
     ptr1 = max_len = 0
     for ptr2, curr_char in enumerate(list(s)):
-        # If character has been encountered before
-        if curr_char in char_dic:
-            # Update ptr_1 if the last instance of this character is closer 
-            # than the last time a character was repeated and updated (makes 
-            # sure we're staying in the current window and not going backwards)
-            ptr1 = max(ptr1, char_dic[curr_char]+1)
+        # If character has been encountered before, update ptr_1 iff the last 
+        # instance of this character is greater than the last time a character 
+        # was repeated and updated (makes sure we're not going backwards)
+        if curr_char in char_dic and char_dic[curr_char + 1] > ptr1:
+            ptr1 = char_dic[curr_char] + 1
         # Add or update current index of character to dict
         char_dic[curr_char] = ptr2
         # Update max len if current max string is longer than max
