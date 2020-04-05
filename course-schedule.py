a28ad6576033da4b6f9da4a050eca6c4b7572981
Carolyn <carolyngychan@gmail.com>
2020-04-05 14:52:27 -0700

    Merged recursion_stack and visited lists together

--- course-schedule.py
+++ course-schedule.py
@@ -1,7 +1,7 @@
 # Course Schedule
-# Check the graph for loops
+# Check the graph for cycles using a variation of topological sort via DFS
+# Time: O(V + E) | Space: O(V + E)
 
-from collections import defaultdict
 from typing import List
 
 def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
@@ -14,32 +14,30 @@ def canFinish(numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list[course].append(prereq)
     
     # Visited list
-    visited = [False] * numCourses 
+    visited = [0] * numCourses 
     
     # Call DFS on each unvisited vertex
     for i in range(numCourses):
-        # Recursion stack is stack of vertices visited during current DFS 
-        # traversal, whereas visited is vertices visited during entire
-        # traversal
-        recursion_stack = [False] * numCourses
         # Call DFS on current vertex 
-        if not(dfs_util(i, adj_list, visited, recursion_stack)):
+        if not(dfs(i, adj_list, visited)):
             return False
     return True
 
-def dfs_util(i: int, adj_list: List[List[int]], visited: List[bool], recursion_stack: List[bool]) -> bool:
-    # If vertex has already been visited before
-    if visited[i]:
-        # If vertex is already in recursion stack, we have a cycle. If not, then vertex was visited in a previous instance of DFS and was found to not contain a cycle
-        return False if recursion_stack[i] else True
-    # Add current vertex to recursion stack
-    recursion_stack[i] = True
-    # Mark current vertex as visited
-    visited[i] = True
+def dfs(i: int, adj_list: List[List[int]], visited: List[int]) -> bool:
+    # If vertex has already been visited during this DFS, meaning a cycle exists
+    if visited[i] == -1:
+        return False 
+    # Vertex was visited in another DFS and found not to have a cycle
+    if visited[i] == 1:
+        return True
+    # Mark current vertex as visited during current DFS
+    visited[i] = -1
     # Perform DFS on all adjacent vertices
     for v in adj_list[i]:
-        if not(dfs_util(v, adj_list, visited, recursion_stack[:])):
+        if not(dfs(v, adj_list, visited)):
             return False
+    # Vertex is determined to not have any cycles
+    visited[i] = 1
     return True
 
 # Driver code
