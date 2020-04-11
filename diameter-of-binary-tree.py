3fa50aa4873a5d9f6f694a943dfb903fb27d1539
Carolyn <carolyngychan@gmail.com>
2020-04-11 13:31:47 -0700

    Comments

--- diameter-of-binary-tree.py
+++ diameter-of-binary-tree.py
@@ -6,17 +6,21 @@ from typing import Tuple
 
 def diameterOfBinaryTree(root: TreeNode) -> int:
     def diam_util(root: TreeNode) -> Tuple[int, int]:
+        # If no root, return max depth 0 and longest path 0
         if not(root):
             return 0, 0
+        # Get the max depth and the longest path of the left node 
         left_depth, left_max = diam_util(root.left)
+        # Get the max depth and the longest path of the right node
         right_depth, right_max = diam_util(root.right)
-        curr_max = left_depth + right_depth + 1
+        # Max path for current node is max depth of left and right nodes plus
+        # current node minus 1 because we're counting edges
+        curr_max = left_depth + right_depth
+        # Return max depth and longest path of current node
         return max(left_depth, right_depth) + 1, max(left_max, right_max, curr_max)
 
-    # Subtract 1 because we're counting edges not nodes
-    res = diam_util(root)[1]
-    return res if not(res) else res - 1
-   
+    return diam_util(root)[1]
+    
 # Driver Code
 head = TreeNode(1)
 curr = head
