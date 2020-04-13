00cfef411927c601dfd3abd4ba3833e50e3e1245
Carolyn <carolyngychan@gmail.com>
2020-04-13 15:26:36 -0700

    Comments

--- contiguous-array.py
+++ contiguous-array.py
@@ -4,19 +4,27 @@
 # was reached before, then we know that the string between those two indices
 # must also equal 0.
 
-from math import inf
 from typing import List
 
 def findMaxLength(nums: List[int]) -> int:
+    # Dictionary where key is total and value is first index of total
     dic = {}
+    # Running sum
     total = 0
+    # Length of longest valid subarray
     max_length = 0
+    # Iterate through entire array
     for i, num in enumerate(nums):
+        # Update sum
         total += -1 if num == 0 else 1
+        # If total is 0, this is longest subarray thus far 
         if total == 0:
-            max_length = max(max_length, i + 1)
+            max_length = i + 1
+        # If we have encountered total before, get the distance between 
+        # instances of total
         elif total in dic:
             max_length = max(max_length, i - dic[total])
+        # If neither, add total to dictionary
         else:
             dic[total] = i
     return max_length
@@ -28,5 +36,4 @@ cases = [   [0, 1],
             [1, 1, 0, 1]
 ]
 for case in cases:
-    print("NEW CASE ---------")
     print(findMaxLength(case))
