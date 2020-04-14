4e1c04f46247727ca0d1fff1ab2134f23e59aa6d
Carolyn <carolyngychan@gmail.com>
2020-04-14 10:06:16 -0700

    Cleaned up code

--- perform-string-shifts.py
+++ perform-string-shifts.py
@@ -7,17 +7,12 @@ from typing import List
 def stringShift(s: str, shift: List[List[int]]) -> str:
     # Keep running total
     total = 0
-    for sh in shift:
-        # Get shift direction and amount from list
-        direction, amount = sh
-        # A shift to the left will be negative, shift to the right is positive
-        direction = -1 if direction == 0 else 1
-        # Add new shift to total
-        total += direction * amount
+    for direction, amount in shift:
+        # Negative if to the left else positive to the right
+        total += -amount if direction == 0 else amount
     # No need to do shifts longer than length of string
     total = total % len(s)
-    # An n shift to the left is equal to shift to the right len(s) - n times
-    total = len(s) - total if total < 0 else total
+    # Apply shift
     return s[-total:] + s[:-total]
 
 # Driver code
