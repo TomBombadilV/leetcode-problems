46e58122f976cf1c1ccfd30a1a04bf547469c687
Carolyn <carolyngychan@gmail.com>
2020-04-15 14:34:13 -0700

    Second method

--- product-of-array-except-self-2.py
+++ product-of-array-except-self-2.py
@@ -1,4 +1,5 @@
 # Product of Array Except Self
+# Re-implementing for Leetcode 30 days of code
 # Method 1: 
 # Create two arrays - one that stores the product of all numbers before, and one
 # that stores the product of all numbers after. The product of all numbers 
@@ -10,6 +11,7 @@
 
 from typing import List
 
+# Method 1
 def productExceptSelf(nums: List[int]) -> List[int]:
     prod_a, prod_b = [1] * len(nums), [1] * len(nums)
     # Array of product of all numbers before
@@ -21,7 +23,19 @@ def productExceptSelf(nums: List[int]) -> List[int]:
     # Return product of two arrays
     return [prod_a[i] * prod_b[i] for i in range(len(nums))]
 
-def productExceptSelf
+# Method 2
+def productExceptSelf(nums: List[int]) -> List[int]:
+    res = [1] * len(nums)
+    # Fill res with product of numbers before
+    for i in range(len(nums) - 1):
+        res[i + 1] = res[i] * nums[i]
+    # Keep track of current product because we can't overwrite array
+    prod = 1
+    # Fill res with product of numbers after
+    for i in reversed(range(1, len(nums))):
+        prod *= nums[i]
+        res[i - 1] = res[i - 1] * prod
+    return res
 
 # Driver code
 l = [1, 2, 3, 4]
