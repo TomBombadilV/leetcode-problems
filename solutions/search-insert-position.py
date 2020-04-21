e99332e86da58e29fd3f5c2d447163184b00819d
Carolyn <carolyngychan@gmail.com>
2020-04-21 11:41:18 -0700

    Search insert position

--- solutions/search-insert-position.py
+++ solutions/search-insert-position.py
@@ -8,4 +8,11 @@ def binary_search(nums: List[int], target: int) -> int:
          
 
 def searchInsert(nums: List[int], target: int) -> int:
-    
+    left, right = 0, len(nums) - 1 
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            
+
