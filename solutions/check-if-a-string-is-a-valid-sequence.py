2ec960f188992049ec01520dbd69c78a0997daa0
Carolyn <carolyngychan@gmail.com>
2020-04-30 13:17:04 -0700

    Check if a string is a valid sequence

--- solutions/check-if-a-string-is-a-valid-sequence.py
+++ solutions/check-if-a-string-is-a-valid-sequence.py
@@ -1,20 +1,21 @@
 # Check If a String is a Valid Sequence from Root to Leaves Path in a Binary Tree
 # Whew what title
-# 
+# Check each node's value against the array, and check its left and right
+# children recursively.
+# Time: O(nodes) | Space: O(log(nodes) * arr)
 
 from treenode import TreeNode
 from typing import List
 
 def isValidSequence(root: TreeNode, arr: List[int]) -> bool: 
-    
-    def recurse(root: TreeNode, arr: List[int]) -> bool:
-        if not(root) or not(arr) or not(root.val == arr[0]):
-            return False
-        if root.val == arr[0] and len(arr) == 1 and not(root.left) and not(root.right):
-            return True
-        return recurse(root.left, arr[1:]) or recurse(root.right, arr[1:])
-
-    return recurse(root, arr)
+    # If root is null, if arr is null, or root value is not correct
+    if not(root) or not(arr) or not(root.val == arr[0]):
+        return False
+    # If root value matches array and root is a leaf
+    if root.val == arr[0] and len(arr) == 1 and not(root.left) and not(root.right):
+        return True
+    # Check left and right children 
+    return isValidSequence(root.left, arr[1:]) or isValidSequence(root.right, arr[1:])
 
 # Driver Code
 root = TreeNode(0)
