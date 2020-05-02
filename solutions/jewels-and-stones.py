352f5dc7c8e8dddded5df62d0d2b668c78a1723c
Carolyn <carolyngychan@gmail.com>
2020-05-02 09:25:28 -0700

    Jewels and Stones

---
+++ solutions/jewels-and-stones.py
@@ -0,0 +1,22 @@
+# Jewels and Stones
+# Add all jewels into a hashset. Iterate through string and increment counter if
+# stone exists in jewel set.
+# Time: O(J + S)
+
+def numJewelsInStones(J: str, S: str) -> int:
+    # Add jewels to set
+    jewels = set(list(J))
+    # Count all stones that are jewels
+    res = 0
+    for s in S:
+        if s in jewels:
+            res += 1
+    return res
+
+# Driver Code
+cases = [ ('aA', 'aAAbbbb'),
+          ('z', 'ZZ')
+]
+for case in cases:
+    J, S = case
+    print(numJewelsInStones(J, S))
