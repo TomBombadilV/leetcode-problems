fb76d404555d1567b3e15b6feb61bae52e56c575
Carolyn <carolyngychan@gmail.com>
2020-05-03 11:46:14 -0700

    General testing function

---
+++ solutions/test.py
@@ -0,0 +1,16 @@
+# General test function that takes in a function and arguments and checks 
+# against expected value
+
+from typing import List
+
+def test(function, cases: List) -> None:
+    """
+    Runs given function with given arguments and checks against expected value.
+    cases parameter MUST be in format (arg1, arg2, ...., expected)
+    """
+    for case in cases:
+        args, expected = case[:-1], case[-1]
+        res = function(*args)
+        # Check result against expected value
+        print("Passed" if res == expected else "{0} failed with {1} expected {2}".\
+              format(*args, res, expected))
