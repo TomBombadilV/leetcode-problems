f43ed8d5ba206d2bee9bdf676edc66a194c44089
Carolyn <carolyngychan@gmail.com>
2020-05-11 16:38:53 -0700

    Flood fill

--- solutions/flood-fill.py
+++ solutions/flood-fill.py
@@ -9,15 +9,21 @@ def floodFill(image: List[List[int]], sr: int, sc: int, newColor: int)\
     
     def fill(image: List[List[int]], sr: int, sc: int, newColor: int, 
              origColor: int) -> List[List[int]]:
+        # Check if indices out of bounds or if color is already new color
+        # or color is not original color
         if not(image) or sr < 0 or sc < 0 or sr >= len(image) or\
             sc >= len(image[0]) or image[sr][sc] == newColor or\
             not(image[sr][sc] == origColor):
             return image
+
+        # Set new color
         image[sr][sc] = newColor
+
+        # Look up, down, left and right
         image = fill(image, sr, sc - 1, newColor, origColor)  # Left
         image = fill(image, sr, sc + 1, newColor, origColor)  # Right
-        image = fill(image, sr - 1, sc, newColor, origColor)  # Above
-        image = fill(image, sr + 1, sc, newColor, origColor)  # Below
+        image = fill(image, sr - 1, sc, newColor, origColor)  # Up
+        image = fill(image, sr + 1, sc, newColor, origColor)  # Down
         return image
 
     return fill(image, sr, sc, newColor, image[sr][sc])
