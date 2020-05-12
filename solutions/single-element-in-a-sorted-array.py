adb418939b0e2afa3c0f03381a0a28f6ddde3339
Carolyn <carolyngychan@gmail.com>
2020-05-12 16:09:42 -0700

    Single element in sorted arary

--- solutions/single-element-in-a-sorted-array.py
+++ solutions/single-element-in-a-sorted-array.py
@@ -28,8 +28,19 @@ def singleNonDuplicate(nums: List[int]) -> int:
                 left = mid + 1
     return -1
 
-def singleNonDuplicate(nums: List[int]) -> int:
-    
+"""def singleNonDuplicate(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1 
+    while left <= right:
+        mid = (left + right) // 2
+        if mid % 2 == 0 and nums[mid] == nums[mid + 1] or\
+           mid % 2 == 1 and nums[mid] == nums[mid - 1]:
+            left = mid + 1
+        elif mid % 2 == 0 and nums[mid] == nums[mid - 1] or\
+             mid % 2 == 1 and nums[mid] == nums[mid + 1]:
+            right = mid - 1
+        else:
+            return nums[mid]
+    return -1"""
 
 # Driver Code
 cases = [([1,2,2,3,3,4,4,5,5], 1),
