fdb9c27a0e9bc1f5d8d9ccc8125b91ddc3b0121f
Carolyn <carolyngychan@gmail.com>
2020-05-17 14:43:26 -0700

    Find all anagrams in a string

--- solutions/find-all-anagrams-in-a-string.py
+++ solutions/find-all-anagrams-in-a-string.py
@@ -1,34 +1,36 @@
 # Find All Anagrams in a String
-#
-#
+# Two pointer method
+# Time: O(s * p) | Space: O(p)
 
 from collections import Counter
+from test import test
 from typing import List
 
-def findAnagrams(s: str, p: str) -> List[int]:
-    dic = Counter(p)
+def findAnagrams(self, s: str, p: str) -> List[int]:
+    if len(p) > len(s) or len(s) == 0 or len(p) == 0:
+        return []
+    count = Counter(p)
+    start = end = 0 
     res = []
-    a_i = 0
-    for i in range(len(s)):
-        c = s[i]
-        if c in dic and dic[c] > 0:
-            dic[c] -= 1
-            if sum(dic.values()) == 0:
-                res.append(a_i)
-                dic[a_i] += 1
-                a_i += 1
+    while start <= end and end < len(s):
+        if s[end] in count:
+            if count[s[end]] > 0:
+                count[s[end]] -= 1
+                if sum(count.values()) == 0:
+                    res.append(start)
+                    count[s[start]] += 1
+                    start += 1
+                end += 1
+            else:
+                count[s[start]] += 1
+                start += 1
         else:
-            #if c in dic:
-            #    dic[c] -= 1
-            a_i = a_i + 1 if c in dic else i + 1
-            dic = Counter(p)
-    return res
+            start = end = end + 1 
+            count = Counter(p)
+    return res 
 
 cases = [
     ('cbaebabacd', 'abc', [0, 6]),
     ('abab', 'ab', [0, 1, 2])
 ]
-for case in cases:
-    s, p, ans = case
-    res = findAnagrams(s, p)
-    print("Passed" if res == ans else "Failed with {1} expected {2}".format(res, ans))
+test(findAnagrams, cases)
