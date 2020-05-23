7dc971ac78651b2ef28c4549ed31354d44a8f6a7
Carolyn <carolyngychan@gmail.com>
2020-05-22 21:37:49 -0700

    Multiply strings

--- solutions/multiply-strings.py
+++ solutions/multiply-strings.py
@@ -6,7 +6,10 @@
 # Karatsuba method
 
 def multiply(num1: str, num2: str) -> str:
-    
+    """
+    Method 1
+    """
+
     # Put larger number as multiplicand, smaller as multiplier
     n, m = max(num1, num2), min(num1, num2)
     # Keep track of order of current digit in multiplier
@@ -31,6 +34,32 @@ def multiply(num1: str, num2: str) -> str:
   
     return str(res)
 
+def multiply_k(num1: str, num2: str) -> str:
+    """
+    Method 2
+    """
+    if int(num1) == 0 or int(num2) == 0:
+        return '0'
+
+    if max(len(num1), len(num2)) == 1:
+        return str(int(num1) * int(num2))
+
+    split = -(-max(len(num1), len(num2)) // 2)
+
+    num1 = num1.rjust(split * 2, '0')
+    num2 = num2.rjust(split * 2, '0')
+
+    a, b = num1[:split], num1[split:]
+    c, d = num2[:split], num2[split:]
+
+    ac = int(multiply_k(a, c))
+    bd = int(multiply_k(b, d))
+
+    abcd = int(multiply_k(str(int(a) + int(b)), str(int(c) + int(d))))
+    abcd = abcd - bd - ac 
+
+    return str((ac * (10 ** (split * 2))) + (abcd * 10 ** split) + bd)
+
 # Driver Code
-multiply('12', '9')
-multiply('123', '456')
+print(multiply_k('12', '9'))
+print(multiply_k('123', '456'))
