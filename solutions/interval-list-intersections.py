bdf267b6891d0db93909831fb28d256ca17f98eb
Carolyn <carolyngychan@gmail.com>
2020-05-23 12:48:26 -0700

    Interval List Intersections

--- solutions/interval-list-intersections.py
+++ solutions/interval-list-intersections.py
@@ -1,21 +1,30 @@
 # Interval List Intersections
+# Iterate through both interval lists, throwing away an interval if its too small
+# and preserving any overlaps.
+# Time: O(A + B) | Space: O(max(A, B))
 
 from typing import List
 
 def intervalIntersection(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
     res = []
-    while A and B:
-        a, b = A[0], B[0]
+    i = j = 0
+    
+    while i < len(A) and j < len(B):
+        a, b = A[i], B[j]
+        # If entire B interval comes before A, ignore B interval
         if b[1] < a[0]:
-            B = B[1:]
+            j += 1
+        # If entire A interval comes before B, ignore A interval
         elif b[0] > a[1]:
-            A = A[1:]
-        else:  # Overlap
+            i += 1
+        # If overlap, save overlap (max start, min end)
+        else:
             res.append([max(a[0], b[0]), min(a[1], b[1])])
+            # Throw away interval that has lower end number
             if b[1] > a[1]:
-                A = A[1:]
+                i += 1
             else:
-                B = B[1:]
+                j += 1
     return res
 
 # Driver Code
