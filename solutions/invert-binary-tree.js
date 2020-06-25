db326dbbdd9043efb5784ad3ff94a7e635cbc46a
Carolyn <carolyngychan@gmail.com>
2020-06-24 20:43:36 -0700

    Invert binary tree - Javascript version

--- solutions/invert-binary-tree.js
+++ solutions/invert-binary-tree.js
@@ -1,15 +1,17 @@
 // Invert Binary Tree
 
 function invertTree (root) {
-    const temp = root.left;
-    root.left = root.right;
-    root.right = temp;
+    if (root) {
+        const temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-    if (root.left) {
-        invertTree(root.left);
-    } 
-    if (root.right) {
-        invertTree(root.right);
+        if (root.left) {
+            invertTree(root.left);
+        } 
+        if (root.right) {
+            invertTree(root.right);
+        }
     }
     return root;
 }
