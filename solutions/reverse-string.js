3191f20e5b63e25972f7d1062c281ff6094e05e2
Carolyn <carolyngychan@gmail.com>
2020-06-24 21:49:03 -0700

    Reverse string in javascript

--- solutions/reverse-string.js
+++ solutions/reverse-string.js
@@ -1,7 +1,10 @@
 // Reverse String
 
 const reverseString = s => {
+    // Don't reverse middle if odd length
     const mid = Math.floor(s.length / 2);
+    
+    // Swap from front and back until middle
     for (let i = 0; i < mid; i++) {
         const temp = s[i];
         s[i] = s[s.length - i - 1];
