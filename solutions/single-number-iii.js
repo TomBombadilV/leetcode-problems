0bf5b05d78e4d71d052a3dc8cc3fa3e07d3500e9
Carolyn <carolyngychan@gmail.com>
2020-07-23 11:51:30 -0700

    Single Number III

---
+++ solutions/single-number-iii.js
@@ -0,0 +1,29 @@
+// Single Number III
+// Method 1: Count the frequency of each number using a dictionary | Space: O(n)
+// Method 2: Use a set and delete if curr num exists in set | Space: O(n)
+// Method 3: Sort and check | Time: O(nlogn)
+// Method 4: XOR wot?
+
+const singleNumber = nums => {
+    // Get two unique numbers xor-ed together
+    let xor = 0;
+    for (let i = 0; i < nums.length; i++) {
+        xor ^= nums[i];
+    }
+
+    // Get most significant set bit
+    const setBit = Math.floor(Math.log2(xor));
+    
+    // Partition xor by set bit
+    let xorA = 0;
+    let xorB = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] & setBit) xorA ^= nums[i];
+        else xorB ^= nums[i];
+    }
+    return [xorA, xorB];
+};
+
+// Driver Code
+const nums = [1,2,1,3,2,5];
+console.log(singleNumber(nums));
