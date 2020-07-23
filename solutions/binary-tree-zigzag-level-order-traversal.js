8ffd64ef2e46834981042836dcfe1a2e4e34d4f5
Carolyn <carolyngychan@gmail.com>
2020-07-23 15:00:24 -0700

    Binary Tree Zigzag Level Order Traversal

--- solutions/binary-tree-zigzag-level-order-traversal.js
+++ solutions/binary-tree-zigzag-level-order-traversal.js
@@ -1,4 +1,6 @@
 // Binary Tree Zigzag Level Order Traversal
+// Modified BFS with keeping track of levels and temporary array of current level
+// nodes, reversed or not whenever level changes.
 
 const TreeNode = require('./tree-node.js');
 
@@ -6,29 +8,41 @@ const zigzagLevelOrder = root => {
     if (!root) {
         return [];
     }
+
+    // Queueueueueuuee
     let q = [[root, 0]];
+    // Are we zigging or zagging
     let zig = true;
     let res = [];
+    // Temporary space to put current level's node values
     let currArr = [];
+    // Keep track of level to see if we are going to a new level
     let prevLevel = 0;
+    
+    // Basic BFS 
     while (q.length > 0) {
         let [curr, level] = q.shift();
-        
+       
+        // If new level, add previous level values to result
         if (level != prevLevel) {
             res.push(zig ? currArr : currArr.reverse());
             zig = !zig;
             currArr = [];
         }
 
+        // Add current val to array
         currArr.push(curr.val);
          
+        // Add left and right nodes of current node
         if (curr.left) q.push([curr.left, level + 1]);
         if (curr.right) q.push([curr.right, level + 1]);
         
+        // Update previous level
         prevLevel = level;
     }
+    // Push array of last level
     res.push(zig ? currArr : currArr.reverse());
-    console.log(res);
+    return res;
 };
 
 // Driver Code
