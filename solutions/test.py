7ef95df70a646859357c4cd6eb552a88834f6c37
Carolyn <carolyngychan@gmail.com>
2020-10-24 09:37:36 -0700

    Updated test to prserve original args

--- solutions/test.py
+++ solutions/test.py
@@ -1,6 +1,7 @@
 # General test function that takes in a function and arguments and checks 
 # against expected value
 
+from copy import deepcopy
 from typing import List
 
 def test(function, cases: List) -> None:
@@ -11,8 +12,8 @@ def test(function, cases: List) -> None:
     for case in cases:
         args, expected = case[:-1], case[-1]
         # Save args for printing in case they are manipulated in function
-        orig_args = args
+        orig_args = deepcopy(args)
         res = function(*args)
         # Check result against expected value
         print("Passed" if res == expected else "{0} failed with {1} expected {2}".\
-              format(args, res, expected))
+              format(orig_args, res, expected))
