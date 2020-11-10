50a40d3c60b79f056b212f0931f14316ec7cb550
Carolyn <carolyngychan@gmail.com>
2020-11-10 15:01:53 -0800

    Add Two Numbers

--- solutions/add_two_numbers.cpp
+++ solutions/add_two_numbers.cpp
@@ -3,6 +3,9 @@
 
 #include "linkedlist.hpp"
 
+/* 
+ * Adds given number of ListNodes with 0 value to the head of given list
+ */
 ListNode* padListHead(ListNode *l1, int padSize) {
     ListNode* head = l1;
     for (unsigned i = 0; i < padSize; i++) {
@@ -11,26 +14,43 @@ ListNode* padListHead(ListNode *l1, int padSize) {
     return head;
 }
 
+/* 
+ * Adds two linked lists together recursively
+ */
 int recurse(ListNode* l1, ListNode* l2, ListNode* res) {
     int sum;
-
+    
+    // If at the tail of both lists, sum will have no carry / no need to recurse
     if (l1->next == NULL && l2->next == NULL) {
         sum = (l1->val + l2->val);
     }
+    // Else, get carry by recursing
     else {
         sum = (l1->val + l2->val + recurse(l1->next, l2->next, res));
     }
+
+    // Create new node to add to result
     ListNode* node = new ListNode(sum % 10);
+    
+    // Add new node right after head, because placeholder head allows us to preserve 
+    // list outside of this function's scope
     node->next = res->next;
     res->next = node;
 
     return sum / 10;
 }
 
+/* 
+ * Pads lists so they are the same length, then calls function
+ * that recursively adds them together.
+ */
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    
+    // Get size of two lists
     int lenL1 = linkedListSize(l1);
     int lenL2 = linkedListSize(l2);
 
+    // Pad the shorter list so it matches the length of longer list
     if (lenL1 > lenL2) {
         l2 = padListHead(l2, lenL1 - lenL2);
     }
@@ -38,16 +58,22 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         l1 = padListHead(l1, lenL2 - lenL1);
     }
 
+    // Create placeholder node for result list so we can pass and
+    // modify it in recursive function
     ListNode* res = new ListNode(-1);
 
+    // Recursively add two lists, get final carry value
     int carry = recurse(l1, l2, res);
 
+    // If carry value, add it to the result list as well (again, after
+    // the placeholder head)
     if (carry) {
         ListNode* node = new ListNode(carry);
         node->next = res->next;
         res->next = node;
     }
-
+    
+    // Return list minus placeholder head
     return res->next;
 }
 
@@ -73,6 +99,9 @@ int main() {
 
     ListNode* l1 = buildList({7, 2, 4, 3});
     ListNode* l2 = buildList({5, 6, 4});
+
+    l1 = buildList({1,2,3,4,5});
+    l2 = buildList({9,8,7});
 
     printLinkedList(l1);
     printLinkedList(l2);
